0aa479f33a6abde7183e8649df79e2e279d92833
apy122 <alvaro.perez532@comunidadunir.net>
2025-04-25 13:31:07 +0200

    Estructura index.html version 1.0

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
-addEventListener("DOMContentLoaded", function () {
-    const h1 = document.createElement("h1");
-    h1.textContent = "Hola";
-    document.body.appendChild(h1);
+//Secciones principales de la pagina
+const header = document.createElement("header");
+const nav = document.createElement("nav");
+const main = document.createElement("main");
+const footer = document.createElement("footer");
+
+//links del nav
+links=[
+    {nombre:"Home", url:"index.html"},
+    {nombre:"Noticias", url:"noticias.html"},
+    {nombre:"Quienes somos", url:"about.html"},
+    {nombre:"publicaciones", url:"publicaciones.html"},
+    {nombre:"Areas de Actuacion", url:"areasActuacion.html"},
+    {nombre:"Contacto", url:"contacto.html"},
+]
+
+//crea el header
+function crearHeader() {
+    const aside = document.createElement("aside");
+    
+    //inserta elementos al header la funcion nav y elementos
+    header.appendChild(crearNav());
+    header.appendChild();
+};
+
+//crea el nav
+function crearNav() {
+   
+    return nav;
+};
+
+//crea el main
+function crearMain() {
+    const section = document.createElement("section");
+    const aside = document.createElement("aside");
+    const article = document.createElement("article");
+};
+
+//crea el footer
+function crearFooter() {
+
+    footer.appendChild();
+};
+
+//une todo al body
+function armarPagina() {
+    document.body.appendChild(header);
+    document.body.appendChild(nav);
+    document.body.appendChild(main);
+};
+
+//ejecuta funciones y Doom
+document.addEventListener("DOMContentLoaded", function(){
+    crearHeader();
+    crearMain();
+    crearFooter();
+    armarPagina();
 });
