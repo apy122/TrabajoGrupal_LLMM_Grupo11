1aaefa62e5aa40713eecb59bb9c98366837724fc
apy122 <alvaro.perez532@comunidadunir.net>
2025-04-26 11:17:49 +0200

    version 1.94

--- js/about.js
+++ js/about.js
@@ -14,7 +14,7 @@ export function crearMain() {
     main.appendChild(h1);
 };
 
-//Cargar todo
+//Cargar todo 
 document.addEventListener("DOMContentLoaded", () => {
     iniciarPaginaSecundaria();
     crearMain();
